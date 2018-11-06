513f4203b0ab2a9c94a81c255bec15bb548138b7
Stephen Kelly <stevekm@users.noreply.github.com>
2018-11-06 17:39:33 -0500

    make lims the default view

--- tuco/urls.py
+++ tuco/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lims/', include('lims.urls'))
+    path('lims/', include('lims.urls')),
+    path('', include('lims.urls'))
 ]

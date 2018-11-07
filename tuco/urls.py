5368a53e7bd766e05ea94c8c6cdef7ee6193f889
Stephen K <steve.at.nyu1@gmail.com>
2018-11-07 12:12:18 -0500

    add tuco home page view

--- tuco/urls.py
+++ tuco/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
+app_name = 'tuco'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lims/', include('lims.urls')),
-    path('', include('lims.urls'))
+    path('', views.HomeView.as_view(), name='home')
 ]
